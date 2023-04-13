6266e2d22ac6524bb00631d99d92310151b8685a
halilserkanuz <halilserkanuz@gmail.com>
2023-04-13 02:08:37 +0100

    profile application implemented

--- popup/index.js
+++ popup/index.js
@@ -1,103 +1,90 @@
 
 //chrome.storage.local.set({ 'token': null })
+let selectedProfile = null;
 
+async function getProfiles(token) {
 
-async function selectCV(id, token) {
-
-    let cvs = await fetch('https://jobstbd.com/api/talent/resume', {
+    let profiles = await fetch('https://jobstbd.com/api/talent/profile', {
         headers: {
             'Authorization': `Bearer ${token}`,
         }
     });
 
-    const { data } = await cvs.json();
-
+    const { data } = await profiles.json();
+    console.log(data)
     if (data.length == 0) {
-        document.querySelector("#cvList").innerHTML = "No CV Found. Please upload your CV first. <a href='https://jobstbd.com/talents/profile' target='_blank'>Click here</a>";
         document.querySelector("#btnApply").disabled = true;
     } else {
-        if (id) {
-            selectedCV = id;
-        }
-        else {
-            selectedCV = data[0].id
-        }
-        let html = "<ul>";
-        for (let i = 0; i < data.length; i++) {
-            html += `<li id="li_${data[i].id}" class="m-2 ${data[i].id == selectedCV ? 'border border-2 border-indigo-100' : ''}"><div class="flex flex-row"><div>${data[i].id == selectedCV ? '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg >' : ''}</div><div class="pt-1 pl-2"> ${data[i].file_name} </div> </li > `;
-        }
-        html += "</ul>";
 
-        document.querySelector("#cvList").innerHTML = html;
-
+        let html = '<select id="profilesSelect" name="profiles" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">';
         for (let i = 0; i < data.length; i++) {
-            var link = document.getElementById(`li_${data[i].id}`);
-            // onClick's logic below:
-            link.addEventListener('click', function () {
-                selectCV(data[i].id, token);
-            });
+            html += `<option value="${data[i].id}" ${i == 0 ? "selected" : ""}>${data[i].name}</option>`;
         }
+        html += "</select>";
+
+        document.querySelector("#profileList").innerHTML = html;
+
         document.querySelector("#btnApply").disabled = false;
     }
 
 }
 
-function addCVUploadChangeEvent(token) {
+// function addCVUploadChangeEvent(token) {
 
-    const fileInput = document.querySelector("#dropzone-file");
+//     const fileInput = document.querySelector("#dropzone-file");
 
-    fileInput.addEventListener("change", async () => {
+//     fileInput.addEventListener("change", async () => {
 
-        if (fileInput.files.length > 0) {
+//         if (fileInput.files.length > 0) {
 
 
-            const file = fileInput.files[0];
-            let data = await fetch(`https://jobstbd.com/api/s3/cvUpload`, {
-                method: "POST",
-                Authorization: `Bearer ${token}`,
-                body: JSON.stringify({
-                    name: `${file?.name}`,
-                    type: file.type,
-                    extension: file.name.split(".")[1]
-                })
-            })
-            const { url, fileName } = await data.json();
+//             const file = fileInput.files[0];
+//             let data = await fetch(`https://jobstbd.com/api/s3/cvUpload`, {
+//                 method: "POST",
+//                 Authorization: `Bearer ${token}`,
+//                 body: JSON.stringify({
+//                     name: `${file?.name}`,
+//                     type: file.type,
+//                     extension: file.name.split(".")[1]
+//                 })
+//             })
+//             const { url, fileName } = await data.json();
 
-            await fetch(url, {
-                method: "PUT",
-                body: file,
-                headers: {
-                    "Content-type": file.type,
-                    "Access-Control-Allow-Origin": "*"
-                }
-            });
+//             await fetch(url, {
+//                 method: "PUT",
+//                 body: file,
+//                 headers: {
+//                     "Content-type": file.type,
+//                     "Access-Control-Allow-Origin": "*"
+//                 }
+//             });
 
-            console.log('resume ', token);
+//             console.log('resume ', token);
 
-            const result = await fetch("https://jobstbd.com/api/talent/resume", {
-                method: "POST",
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    file_name: file.name,
-                    s3_file_name: `${fileName}`,
-                }),
-            });
+//             const result = await fetch("https://jobstbd.com/api/talent/resume", {
+//                 method: "POST",
+//                 headers: {
+//                     'Authorization': `Bearer ${token}`,
+//                 },
+//                 body: JSON.stringify({
+//                     file_name: file.name,
+//                     s3_file_name: `${fileName}`,
+//                 }),
+//             });
 
-            if (result.status == 404) {
+//             if (result.status == 404) {
 
-                // chrome.storage.local.set({ 'token': null })
+//                 // chrome.storage.local.set({ 'token': null })
 
-                main()
-            }
+//                 main()
+//             }
 
-            selectCV(null, token)
+//             selectCV(null, token)
 
-        }
+//         }
 
-    });
-}
+//     });
+// }
 
 
 function loadingActive() {
@@ -126,12 +113,13 @@ async function applyJob(token) {
     applyButton.addEventListener("click", async () => {
         chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
             const url = tabs[0].url;
+            const selectedProfile = document.getElementById("profilesSelect").value;
             let response;
             try {
 
                 response = await fetch('https://jobstbd.com/api/talent/application', {
                     method: 'POST',
-                    body: JSON.stringify({ url: url, cvId: selectedCV }),
+                    body: JSON.stringify({ url: url, profileId: selectedProfile }),
                     headers: {
                         'Authorization': `Bearer ${token} `,
                     }
@@ -160,6 +148,7 @@ function main() {
         loadingActive();
 
         const token = result.token;
+        console.log('token ', token);
 
 
         if (result.token === null || result.token === undefined || result.token === "") {
@@ -176,11 +165,11 @@ function main() {
 
                 if (tokenValidate.status === 200) {
 
-                    await selectCV(null, token);
+                    await getProfiles(token);
 
                     await applyJob(token);
 
-                    addCVUploadChangeEvent(token);
+                    // addCVUploadChangeEvent(token);
 
                     await signedInActive();
                 } else {
